8e2a85aa8a1c523cab7e345226f8cfe2a18b9024
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2013-07-29 14:36:36 +0900

    少しRubyらしいコードへ書き換

--- atnd_tweet.rb
+++ atnd_tweet.rb
@@ -5,17 +5,17 @@ require 'twitter'
 require './lib/atnd.rb'
 require './lib/decrypt.rb'
 
-consumer_key = decrypt(File::open('./consumer').readlines[0].chomp, 'MIIEogIBAAKCAQEApvLgIz/F70HA9237')
-consumer_secret = decrypt(File::open('./consumer').readlines[1].chomp, 's/nEcZgrNcxPTCpuXpxGpZAPHny8WVog')
+consumer_key = decrypt(File.open('./consumer').readlines[0].chomp, 'MIIEogIBAAKCAQEApvLgIz/F70HA9237')
+consumer_secret = decrypt(File.open('./consumer').readlines[1].chomp, 's/nEcZgrNcxPTCpuXpxGpZAPHny8WVog')
 
-if !File.exist?('./access') then
+unless File.exist?('./access')
   consumer = OAuth::Consumer.new(consumer_key, consumer_secret, site: 'https://api.twitter.com')
   request_token = consumer.get_request_token
   puts request_token.authorize_url
   print "PIN code: "
   pin = gets.to_i
   access_token = request_token.get_access_token(oauth_verifier: pin)
-  open('./access', 'w') do |f|
+  File.open('./access', 'w') do |f|
     f.puts(access_token.token)
     f.puts(access_token.secret)
   end
