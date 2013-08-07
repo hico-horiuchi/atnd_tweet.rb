b9e5b8d9b710ffb7a688cf9bb9228aedbe708f29
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-08-07 11:07:28 +0900

    直下以外でも実行可能なように修正

--- atnd_tweet.rb
+++ atnd_tweet.rb
@@ -2,20 +2,25 @@ ENV['BUNDLE_GEMFILE'] = File.expand_path(File.join(File.dirname($0), "Gemfile"))
 require 'bundler/setup'
 require 'oauth'
 require 'twitter'
-require './lib/atnd.rb'
-require './lib/decrypt.rb'
 
-consumer_key = decrypt(File.open('./consumer').readlines[0].chomp, 'MIIEogIBAAKCAQEApvLgIz/F70HA9237')
-consumer_secret = decrypt(File.open('./consumer').readlines[1].chomp, 's/nEcZgrNcxPTCpuXpxGpZAPHny8WVog')
+atnd_tweet_directory = File.expand_path(File.dirname(__FILE__))
+require File.expand_path(File.join(atnd_tweet_directory, 'lib/atnd.rb' ))
+require File.expand_path(File.join(atnd_tweet_directory, 'lib/decrypt.rb'))
 
-unless File.exist?('./access')
+consumer_file = File.expand_path(File.join(atnd_tweet_directory, 'consumer' ))
+access_file = File.expand_path(File.join(atnd_tweet_directory, 'access' ))
+
+consumer_key = decrypt(File.open(consumer_file).readlines[0].chomp, 'MIIEogIBAAKCAQEApvLgIz/F70HA9237')
+consumer_secret = decrypt(File.open(consumer_file).readlines[1].chomp, 's/nEcZgrNcxPTCpuXpxGpZAPHny8WVog')
+
+unless File.exist?(access_file)
   consumer = OAuth::Consumer.new(consumer_key, consumer_secret, site: 'https://api.twitter.com')
   request_token = consumer.get_request_token
   puts request_token.authorize_url
   print "PIN code: "
   pin = gets.to_i
   access_token = request_token.get_access_token(oauth_verifier: pin)
-  File.open('./access', 'w') do |f|
+  File.open(access_file, 'w') do |f|
     f.puts(access_token.token)
     f.puts(access_token.secret)
   end
@@ -24,8 +29,8 @@ end
 Twitter.configure do |config|
   config.consumer_key = consumer_key
   config.consumer_secret = consumer_secret
-  config.oauth_token = File::open('./access').readlines[0].chomp
-  config.oauth_token_secret = File::open('./access').readlines[1].chomp
+  config.oauth_token = File.open(access_file).readlines[0].chomp
+  config.oauth_token_secret = File.open(access_file).readlines[1].chomp
 end
 
 atnd = Atnd.new(ARGV[0])
