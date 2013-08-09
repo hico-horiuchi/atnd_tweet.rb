6bc1ca0fc842535abfce76c81b9efa4d1f594662
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-08-09 20:43:51 +0900

    IO.readlinesを使うように修正

--- atnd_tweet.rb
+++ atnd_tweet.rb
@@ -4,33 +4,35 @@ require 'oauth'
 require 'twitter'
 
 atnd_tweet_directory = File.expand_path(File.dirname(__FILE__))
-require File.expand_path(File.join(atnd_tweet_directory, 'lib/atnd.rb' ))
+require File.expand_path(File.join(atnd_tweet_directory, 'lib/atnd.rb'))
 require File.expand_path(File.join(atnd_tweet_directory, 'lib/decrypt.rb'))
 
-consumer_file = File.expand_path(File.join(atnd_tweet_directory, 'consumer' ))
-access_file = File.expand_path(File.join(atnd_tweet_directory, 'access' ))
+consumer_file_name = File.expand_path(File.join(atnd_tweet_directory, 'consumer'))
+access_file_name = File.expand_path(File.join(atnd_tweet_directory, 'access'))
 
-consumer_key = decrypt(File.open(consumer_file).readlines[0].chomp, 'MIIEogIBAAKCAQEApvLgIz/F70HA9237')
-consumer_secret = decrypt(File.open(consumer_file).readlines[1].chomp, 's/nEcZgrNcxPTCpuXpxGpZAPHny8WVog')
+consumer_file = IO.readlines(consumer_file_name)
+consumer_key = decrypt(consumer_file[0].chomp, 'MIIEogIBAAKCAQEApvLgIz/F70HA9237')
+consumer_secret = decrypt(consumer_file[1].chomp, 's/nEcZgrNcxPTCpuXpxGpZAPHny8WVog')
 
-unless File.exist?(access_file)
+unless File.exist?(access_file_name)
   consumer = OAuth::Consumer.new(consumer_key, consumer_secret, site: 'https://api.twitter.com')
   request_token = consumer.get_request_token
   puts request_token.authorize_url
   print "PIN code: "
   pin = gets.to_i
   access_token = request_token.get_access_token(oauth_verifier: pin)
-  File.open(access_file, 'w') do |f|
+  File.open(access_file_name, 'w') do |f|
     f.puts(access_token.token)
     f.puts(access_token.secret)
   end
 end
 
+access_file = IO.readlines(access_file_name)
 Twitter.configure do |config|
   config.consumer_key = consumer_key
   config.consumer_secret = consumer_secret
-  config.oauth_token = File.open(access_file).readlines[0].chomp
-  config.oauth_token_secret = File.open(access_file).readlines[1].chomp
+  config.oauth_token = access_file[0].chomp
+  config.oauth_token_secret = access_file[1].chomp
 end
 
 atnd = Atnd.new(ARGV[0])
